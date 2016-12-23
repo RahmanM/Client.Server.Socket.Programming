866f3ca5b4f79014abd41d1ce41d9ed98872a084
Rahman Mahmoodi <rahman.mahmoodi@gmail.com>
2016-12-23 12:39:30 +1100

    first commit

---
+++ App.Client/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace App.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = null;
+
+            try
+            {
+                tcpClient = new TcpClient();
+                Console.WriteLine("Connecting ...");
+                tcpClient.Connect("192.168.1.107", 8001);
+                Console.WriteLine("Connected ...");
+
+                var asciiEncoding = new ASCIIEncoding();
+
+                // Send login to service
+                var login = asciiEncoding.GetBytes("Rahman/Mahmoodi");
+                var networkStream = tcpClient.GetStream();
+                networkStream.Write(login, 0, login.Length);
+                // End sending message to server
+
+                #region Read All Server Message
+
+                while (true)
+                {
+                    if (networkStream.CanRead)
+                    {
+                        ////Buffer to store the response bytes.
+                        byte[] readBuffer = new byte[tcpClient.ReceiveBufferSize];
+
+                        // Initiate a read of zero bytes. Note: without this it won't work!
+                        //var bytes = new byte[0];
+                        //networkStream.Read(bytes, 0, 0);
+                        var fullServerReply = string.Empty;
+
+                        using (var writer = new MemoryStream())
+                        {
+                            while (networkStream.DataAvailable)
+                            {
+                                int numberOfBytesRead = networkStream.Read(readBuffer, 0, readBuffer.Length);
+
+                                if (numberOfBytesRead <= 0)
+                                {
+                                    break;
+                                }
+                                writer.Write(readBuffer, 0, numberOfBytesRead);
+                            }
+
+                            fullServerReply = Encoding.UTF8.GetString(writer.ToArray());
+                        }
+
+
+                        Console.WriteLine(fullServerReply);
+
+                        Thread.Sleep(2000); // Sleep for demo purpuses!!!
+                        //Console.WriteLine("Press enter to read next message...");
+                        //Console.ReadLine();
+
+                    }
+                }
+
+                #endregion
+
+                //// Note: this is fixed buffer size
+                #region Fixed Buffer
+
+                //StringBuilder totalString = new StringBuilder();
+
+                //// Read responses from server. Note: Working
+                //while (true)
+                //{
+                //    var bytes = new byte[4000];
+                //    var bufferSize = networkStream.Read(bytes, 0, 4000);
+
+                //    for (int i = 0; i < bufferSize; i++)
+                //    {
+                //        Console.WriteLine(Convert.ToChar(bytes[i]));
+                //        totalString.Append(Convert.ToChar(bytes[i]));
+                //    }
+
+                //    Console.WriteLine(totalString.ToString());
+                //    totalString.Clear();
+                //    //Console.ReadLine();
+                //}
+
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (tcpClient != null) tcpClient.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Server.App/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener=null;
+            Socket socket=null;
+
+            try
+            {
+                var iPAddress = IPAddress.Parse("192.168.1.107");
+
+                // initialise listener
+                listener = new TcpListener(iPAddress, 8001);
+                listener.Start();
+
+                Console.WriteLine("The server is running at port 8001");
+                Console.WriteLine("The local end point is: " + listener.LocalEndpoint);
+
+                socket = listener.AcceptSocket();
+                Console.WriteLine("Connection accepted from : " + socket.RemoteEndPoint);
+
+                var bytes = new byte[100];
+                var bufferSize = socket.Receive(bytes);
+                for (int i = 0; i < bufferSize; i++)
+                {
+                    Console.WriteLine(Convert.ToChar(bytes[i]));
+                }
+
+                Console.WriteLine("Received ...");
+
+                var asciiEncoding = new ASCIIEncoding();
+                socket.Send(asciiEncoding.GetBytes("ACK"));
+
+                var fixString = "8=FIX.4.19=6135=A34=149=EXEC52=20121105-23:24:0656=BANZAI98=0108=3010=0038=FIX.4.19=6135=A34=149=BANZAI52=20121105-23:24:0656=EXEC98=0108=3010=0038=FIX.4.19=4935=034=249=BANZAI52=20121105-23:24:3756=EXEC10=2288=FIX.4.19=4935=034=249=EXEC52=20121105-23:24:3756=BANZAI10=2288=FIX.4.19=10335=D34=349=BANZAI52=20121105-23:24:4256=EXEC11=135215788257721=138=1000040=154=155=MSFT59=010=0628=FIX.4.19=13935=834=349=EXEC52=20121105-23:24:4256=BANZAI6=011=135215788257714=017=120=031=032=037=138=1000039=054=155=MSFT150=2151=010=0598=FIX.4.19=15335=834=449=EXEC52=20121105-23:24:4256=BANZAI6=12.311=135215788257714=1000017=220=031=12.332=1000037=238=1000039=254=155=MSFT150=2151=010=2308=FIX.4.19=10335=D34=449=BANZAI52=20121105-23:24:5556=EXEC11=135215789503221=138=1000040=154=155=ORCL59=010=0478=FIX.4.19=13935=834=549=EXEC52=20121105-23:24:5556=BANZAI6=011=135215789503214=017=320=031=032=037=338=1000039=054=155=ORCL150=2151=010=0498=FIX.4.19=15335=834=649=EXEC52=20121105-23:24:5556=BANZAI6=12.311=135215789503214=1000017=420=031=12.332=1000037=438=1000039=254=155=ORCL150=2151=010=2208=FIX.4.19=10835=D34=549=BANZAI52=20121105-23:25:1256=EXEC11=135215791235721=138=1000040=244=1054=155=SPY59=010=0038=FIX.4.19=13835=834=749=EXEC52=20121105-23:25:1256=BANZAI6=011=135215791235714=017=520=031=032=037=538=1000039=054=155=SPY150=2151=010=2528=FIX.4.19=10435=F34=649=BANZAI52=20121105-23:25:1656=EXEC11=135215791643738=1000041=135215791235754=155=SPY10=1988=FIX.4.19=8235=334=849=EXEC52=20121105-23:25:1656=BANZAI45=658=Unsupported message type10=0008=FIX.4.19=10435=F34=749=BANZAI52=20121105-23:25:2556=EXEC11=135215792530938=1000041=135215791235754=155=SPY10=1978=FIX.4.19=8235=334=949=EXEC52=20121105-23:25:2556=BANZAI45=758=Unsupported message type10=002";
+                socket.Send(asciiEncoding.GetBytes(fixString));
+                
+                // Simulate to send data continiously
+                //while (true)
+                //{
+                //    socket.Send(asciiEncoding.GetBytes(fixString));
+                //    Thread.Sleep(1000);
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (socket != null) socket.Close();
+                if (listener != null) listener.Stop();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
